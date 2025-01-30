0f5bbad3f23c05285a50c59e899c5ee0aee0ee30
danicex <dantegaefe@gmail.com>
2025-01-30 20:10:03 +0100

    created slide

--- client/src/Componenets/Banner.jsx
+++ client/src/Componenets/Banner.jsx
@@ -1,36 +1,43 @@
-import React from 'react'
+import React from 'react';
 import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-
+import image1 from '../assets/angel_store_image001.png';
+import image2 from '../assets/angel_store_image002.png';
+import image3 from '../assets/angel_store_image003.png';
+import image4 from '../assets/angel_store_image004.png';
 
 export default function Banner() {
-    const headerData = [
-        { title: 'Title 1' },
-        { title: 'Title 2' },
-        { title: 'Title 3' },
-        { title: 'Title 4' },
-      ];
+  const headerData = [
+    { image: image1 },
+    { image: image2 },
+    { image: image3 },
+    { image: image4 },
+  ];
+
   return (
-<div className="swiper-wrapper my-5">
-        <Swiper
-          className="swiper-container"
-          modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
-          spaceBetween={20}
-          slidesPerView={1}
-          autoplay={{ delay: 10000 }}
-          pagination={{ clickable: true }}
-        >
-          {headerData.map((item, index) => (
-            <SwiperSlide key={index}>
-              <div className="banner-image w-full">
-                <h1 className="text-2xl text-center">{item.title}</h1>
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>  
-    )
+    <div className="">
+      <Swiper
+        className="swiper-container"
+        modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
+        spaceBetween={20}
+        slidesPerView={1}
+        autoplay={{ delay: 10000, disableOnInteraction: false }}
+        pagination={{ clickable: true }}
+        navigation
+      >
+        {headerData.map((item, index) => (
+          <SwiperSlide key={index}>
+                   <img
+      src={item.image}
+      alt={`Slide ${index + 1}`}
+      className='m-auto'
+    />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
 }
